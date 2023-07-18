a1dcd28afa29a0fbf135979035927616a69ca0b9
Ottispottis <osheld@utu.fi>
2023-07-18 22:14:20 +0300

    Delete updateForm.tsx

--- src/helpers/updateForm.tsx
+++
@@ -1,5 +0,0 @@
-export function updateForm(key: string){
-    return function(e: React.ChangeEvent<HTMLInputElement>){
-        setForm(prev =>({
-            ...prev, key: e.target.value}))}
-    }
