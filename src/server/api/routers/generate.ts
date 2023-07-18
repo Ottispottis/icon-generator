0cb3d298b6e87136a647bdd9e241b5e68e1b92a7
Ottispottis <osheld@utu.fi>
2023-07-18 21:37:40 +0300

    renamed some env variables

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -13,8 +13,8 @@ import { env } from "~/env.mjs";
 
 const s3 = new AWS.S3({
     credentials:{
-        accessKeyId: env.AWS_ACCESS_ID,
-        secretAccessKey: env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: env.ACCESS_KEY_ID,
+        secretAccessKey: env.SECRET_ACCESS_KEY
     },
     region: "eu-north-1"
 });
